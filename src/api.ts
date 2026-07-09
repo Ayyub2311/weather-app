cffd1ad68aea2232ea74aabd7e220fd382faaed9
Ayyub2311 <ayyubkhondjumabaev@gmail.com>
2026-07-09 10:33:27 +0500

    last components

--- src/api.ts
+++ src/api.ts
@@ -22,7 +22,7 @@ return WeatherSchema.parse(data)
 
 export async function getGeocode(location: string) {
     const res = await fetch(
-        `http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=1&appid=${API_KEY}`
+        `https://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=1&appid=${API_KEY}`
     ) 
 const data = await res.json()
 
@@ -31,7 +31,7 @@ return GeocodeSchema.parse(data)
 
 export async function getAirPollution({ lat, lon }: { lat: number; lon: number }) {
     const res = await fetch(
-        `http://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=${API_KEY}`
+        `https://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=${API_KEY}`
     ) 
 const data = await res.json()
 
